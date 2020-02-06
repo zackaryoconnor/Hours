63c456d3ac11f7fca1f2e7d87c70be247026278a
Zackary O'Connor <zackaryoconnor@Zackarys-iMac.local>
2020-02-05 22:38:56 -0500

    Setup basic tableview.

---
+++ Hours/HomeController.swift
@@ -0,0 +1,72 @@
+//
+//  HomeController.swift
+//  Hours
+//
+//  Created by Zackary O'Connor on 2/5/20.
+//  Copyright © 2020 Zackary O'Connor. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UITableViewController {
+    
+    fileprivate let cellId = "cellId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavBar()
+        setupTableView()
+    }
+    
+    
+    fileprivate func setupTableView() {
+        tableView.register(HomeCell.self, forCellReuseIdentifier: cellId)
+        tableView.backgroundColor = .systemBackground
+        tableView.rowHeight = 75
+        tableView.tableFooterView = UIView()
+        tableView.allowsSelection = false
+    }
+    
+    
+    fileprivate func setupNavBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+        navigationItem.rightBarButtonItem?.tintColor = .systemGreen
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Hours"
+    }
+    
+}
+
+
+
+
+extension HomeController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId) as! HomeCell
+        return cell
+    }
+}
+
+
+
+
+
+
+
+class HomeCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
